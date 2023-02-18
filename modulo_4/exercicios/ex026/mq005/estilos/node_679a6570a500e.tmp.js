6cd70ab85f1dfb2269d41995dcf346c93f2a1135
Dahe Kevin Ripardo Feitosa <94403462+Dahedev@users.noreply.github.com>
2023-02-18 11:54:35 -0300

    criando menu

---
+++ modulo_4/exercicios/ex026/mq005/estilos/node_679a6570a500e.tmp.js
@@ -0,0 +1,37 @@
+@charset "UTF-8";
+
+* {
+    margin: 0px;
+    padding: 0px;
+    font-family: Arial, Helvetica, sans-serif;
+}
+
+html, body {
+    background-color: lightgrey;
+}
+
+header {
+    background-color: darkgrey;
+    text-align: center;
+    font-size: 1.5em;
+}
+
+menu {
+    text-align: left;
+    padding: 10px;
+}
+
+menu > ul > li {
+    display: inline;
+    padding: 10px;
+    background-color: darkslategrey;
+}
+
+menu a {
+    text-decoration: none;
+    color: white;
+}
+
+article {
+    width: 8;
+}
